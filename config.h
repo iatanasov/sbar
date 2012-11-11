6605fdf99e276ac56d1d335c51fa66d249dc3fc6
iatanasov <iatanasov@vaio.ivan-bg.org>
2012-11-11 11:04:21 -0600

    add battery

--- config.h
+++ config.h
@@ -1,9 +1,16 @@
-
-
 #define SLEEP_NUMBER 1
 
+/* TODO 
+ when using sys basic battery info can be found in /sys/class/power_supply/BAT0/uevent
+    
+ 
+ */
 char *config[] = { 
     "/proc/acpi/battery/BAT0/state",
     "/proc/acpi/ibm/volume",
 };
 
+char *bat_files[] = {
+    "/proc/acpi/battery/BAT%d/state",
+    "/sys/class/power_supply/BAT%d/uevent",
+};
